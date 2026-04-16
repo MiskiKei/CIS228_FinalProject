7642e670c2d51bde800bbda6be1f9cb40b9783a3
daws <crestsorcerer@gmail.com>
2026-04-15 20:12:12 -0400

    I think I added mostly everything, or at least enough for you to get started

---
+++ src/Book.java
@@ -0,0 +1,39 @@
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private int quantity;
+
+    public Book(String title, String author, String isbn, int quantity) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.quantity = quantity;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public String getISBN() {
+        return isbn;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public void setISBN(String isbn) {
+        this.isbn = isbn;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
